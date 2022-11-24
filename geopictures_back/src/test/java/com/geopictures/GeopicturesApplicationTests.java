f27b34d78b2b32fd073c0506d07aea34ad358b12
Anthony Bonnet <anthony.bonnet1@orange.fr>
2022-11-24 15:57:46 +0100

    Creation projet sdk 17

---
+++ geopictures_back/src/test/java/com/geopictures/GeopicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geopictures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeopicturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
