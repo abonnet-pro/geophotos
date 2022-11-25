b751119223de6c1d89ed6b507679aa5bcfab00c9
Voltext <lokin84250@live.fr>
2022-11-26 00:28:29 +0100

    Base Google Authentication

--- geopictures_front/App.js
+++ geopictures_front/App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Button, Image } from "react-native";
+import {
+  Box,
+  Heading,
+  AspectRatio,
+  Text,
+  Center,
+  HStack,
+  Stack,
+  NativeBaseProvider,
+} from "native-base";
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+
+  const [accessToken, setAccessToken] = React.useState();
+  const [userInfo, setUserInfo] = React.useState();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: "549351708019-q9d71fkan3u185bfsg1b3j2svg9ccpob.apps.googleusercontent.com",
+    
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      setAccessToken(response.authentication.accessToken)
+      accessToken && getUserData();
+    }
+  }, [response, accessToken]);
+
+  async function getUserData() {
+    let userInfoReponse = await fetch("https://www.googleapis.com/userinfo/v2/me" , {
+      headers:  { Authorization: `Bearer ${accessToken}` }
+    })
+    const useInfo = await userInfoReponse.json();
+    setUserInfo(useInfo)
+    
+    console.log(useInfo.email)
+  }
+
+  function showUserInfo() {
+    if(userInfo) {
+      return (
+        <>
+        <View>
+          <Image source={{uri: userInfo.picture}} alt='profil' />
+          <Text>Bonjour {userInfo.name}</Text>
+          <Text>Votre mail {userInfo.email}</Text>
+        </View>
+        </>
+      )
+    }
+    else {
+      return (
+        <Text>Test</Text>
+      )
+    }
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        {showUserInfo()}
+        <Button 
+        title={accessToken ? "Get info" : "Login"}
+        onPress={accessToken ? getUserData : () => { promptAsync({showInRecents : true})}}
+    />
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
